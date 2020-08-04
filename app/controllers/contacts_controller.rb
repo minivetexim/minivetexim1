5ce4379fa91cd4ac709863b04362e44a00283b74
minivetexim <minivetexim@gmail.com>
2020-08-05 04:07:56 +0530

    update

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,39 @@
+class ContactsController < ApplicationController
+
+
+before_action :find_contact, only: [:show, :edit, :update, :destroy]
+
+def index   
+ @contacts = Contact.order(created_at: :desc)
+end
+
+def create
+ @contacts = Contact.new(contact_params)
+    if @contacts.save
+       redirect_to homes_path 
+    end
+end
+
+def minivet_exim903535
+	
+ @contacts = Contact.all
+
+end
+
+
+private
+
+def contact_params
+			
+	params.require(:contact).permit(:name,:phone_number,:email,:message)
+
+end
+
+def find_contact
+    	 
+    @contact = Contact.find(params[:id])
+ 
+end
+
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+root :to => "homes#index"
+
+resources :homes
+resources :contacts
+get '/minivet_exim903535' => 'contacts#minivet_exim903535'
+
+end
