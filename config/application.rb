df173c0ae0bdac87f0d38e52b30eb3b3c427d1bf
minivetexim <minivetexim@gmail.com>
2020-08-05 18:59:13 +0530

    upadate

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Minivetexim
     config.load_defaults 5.2
     config.assets.paths << "#{Rails.root}/app/assets/videos"
 
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
